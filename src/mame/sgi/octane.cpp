7eb5614e5ee16baa37c93030a5ca020e0da0ff72
MAME-OCTANE <mamamaycookie@outlook.com>
2025-08-04 18:14:12 +0200

    Update octane.cpp

--- src/mame/sgi/octane.cpp
+++ src/mame/sgi/octane.cpp
@@ -9,7 +9,7 @@
     Memory map (early WIP):
     00000000 - 0fffffff : RAM (256MB)
     1fc00000 - 1fcfffff : Boot PROM
-    1f800000 - 1fbfffff : HEART (stub)
+    1f800000 - 1fbfffff : HEART (device)
     1f000000 - 1f3fffff : XBow (stub)
     1f120000 - 1f12ffff : IOC3 (stub)
 
@@ -20,6 +20,8 @@
 #include "machine/ram.h"
 #include "machine/serial.h"
 #include "machine/rs232.h"
+#include "machine/heart.h"
+#include "machine/address_map_bank.h"
 
 #define LOG_UNKNOWN     (1U << 1)
 #define LOG_ALL         (LOG_UNKNOWN)
@@ -35,6 +37,7 @@ public:
 	octane_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag)
 		, m_maincpu(*this, "maincpu")
+		, m_heart(*this, "heart")
 	{
 	}
 
@@ -44,21 +47,14 @@ protected:
 	void mem_map(address_map &map) ATTR_COLD;
 
 	required_device<r10000be_device> m_maincpu;
+	required_device<heart_device> m_heart;
 };
 
 void octane_state::mem_map(address_map &map)
 {
 	map(0x00000000, 0x0fffffff).ram(); // 256MB RAM
 	map(0x1fc00000, 0x1fcfffff).rom().region("user1", 0); // Boot PROM
-
-	// Stubbed devices with log-only handlers
-	map(0x1f800000, 0x1fbfffff).lrw8([](address_space &space, offs_t offset) {
-		logerror("HEART: Read @ %08x\n", offset);
-		return uint8_t(0);
-	});
-	map(0x1f800000, 0x1fbfffff).lrw8([](address_space &space, offs_t offset, uint8_t data) {
-		logerror("HEART: Write @ %08x = %02x\n", offset, data);
-	});
+	map(0x1f800000, 0x1fbfffff).m("heart_map", AS_PROGRAM); // Heart memory-mapped region
 
 	map(0x1f000000, 0x1f3fffff).lrw8([](address_space &space, offs_t offset) {
 		logerror("XBOW: Read @ %08x\n", offset);
@@ -86,6 +82,10 @@ void octane_state::octane(machine_config &config)
 	m_maincpu->set_icache_size(32768);
 	m_maincpu->set_dcache_size(32768);
 	m_maincpu->set_addrmap(AS_PROGRAM, &octane_state::mem_map);
+
+	HEART(config, m_heart, 0);
+
+	ADDRESS_MAP_BANK(config, "heart_map").set_map(&heart_device::map).set_options(ENDIANNESS_BIG, 32, 22, 0);
 
 	rs232_port_device &rs232(RS232_PORT(config, "rs232", default_rs232_devices, "terminal"));
 	rs232.rxd_handler().set("ser", FUNC(serial_port_device::rx));
