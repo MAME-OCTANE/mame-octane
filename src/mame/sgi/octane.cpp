615739a14f9e57c0af323b92003f7f3fe06aa14e
MAME-OCTANE <mamamaycookie@outlook.com>
2025-08-04 18:03:30 +0200

    Update octane.cpp

--- src/mame/sgi/octane.cpp
+++ src/mame/sgi/octane.cpp
@@ -2,24 +2,30 @@
 // copyright-holders:Ryan Holtz
 /**********************************************************************
 
-    SGI Octane workstation skeleton driver
+    SGI Octane workstation driver (WIP)
 
-    To Do: Everything
+    GOAL: Boot SGI IP30 PROM and reach IRIX installer
 
-    Memory map:
-    1fc00000 - 1fc7ffff      Boot ROM
+    Memory map (early WIP):
+    00000000 - 0fffffff : RAM (256MB)
+    1fc00000 - 1fcfffff : Boot PROM
+    1f800000 - 1fbfffff : HEART (stub)
+    1f000000 - 1f3fffff : XBow (stub)
+    1f120000 - 1f12ffff : IOC3 (stub)
 
 **********************************************************************/
 
 #include "emu.h"
 #include "cpu/mips/mips3.h"
+#include "machine/ram.h"
+#include "machine/serial.h"
+#include "machine/rs232.h"
 
 #define LOG_UNKNOWN     (1U << 1)
 #define LOG_ALL         (LOG_UNKNOWN)
 
 #define VERBOSE         (0)
 #include "logmacro.h"
-
 
 namespace {
 
@@ -37,12 +43,38 @@ public:
 protected:
 	void mem_map(address_map &map) ATTR_COLD;
 
-	required_device<r5000be_device> m_maincpu;
+	required_device<r10000be_device> m_maincpu;
 };
 
 void octane_state::mem_map(address_map &map)
 {
-	map(0x1fc00000, 0x1fcfffff).rom().region("user1", 0);
+	map(0x00000000, 0x0fffffff).ram(); // 256MB RAM
+	map(0x1fc00000, 0x1fcfffff).rom().region("user1", 0); // Boot PROM
+
+	// Stubbed devices with log-only handlers
+	map(0x1f800000, 0x1fbfffff).lrw8([](address_space &space, offs_t offset) {
+		logerror("HEART: Read @ %08x\n", offset);
+		return uint8_t(0);
+	});
+	map(0x1f800000, 0x1fbfffff).lrw8([](address_space &space, offs_t offset, uint8_t data) {
+		logerror("HEART: Write @ %08x = %02x\n", offset, data);
+	});
+
+	map(0x1f000000, 0x1f3fffff).lrw8([](address_space &space, offs_t offset) {
+		logerror("XBOW: Read @ %08x\n", offset);
+		return uint8_t(0);
+	});
+	map(0x1f000000, 0x1f3fffff).lrw8([](address_space &space, offs_t offset, uint8_t data) {
+		logerror("XBOW: Write @ %08x = %02x\n", offset, data);
+	});
+
+	map(0x1f120000, 0x1f12ffff).lrw8([](address_space &space, offs_t offset) {
+		logerror("IOC3: Read @ %08x\n", offset);
+		return uint8_t(0);
+	});
+	map(0x1f120000, 0x1f12ffff).lrw8([](address_space &space, offs_t offset, uint8_t data) {
+		logerror("IOC3: Write @ %08x = %02x\n", offset, data);
+	});
 }
 
 static INPUT_PORTS_START( octane )
@@ -50,10 +82,16 @@ INPUT_PORTS_END
 
 void octane_state::octane(machine_config &config)
 {
-	R5000BE(config, m_maincpu, 50000000*4); // NOTE: Wrong - should be R10000BE!
-	m_maincpu->set_icache_size(32768);      // Unknown CPU cache size
+	R10000BE(config, m_maincpu, 195'000'000);
+	m_maincpu->set_icache_size(32768);
 	m_maincpu->set_dcache_size(32768);
 	m_maincpu->set_addrmap(AS_PROGRAM, &octane_state::mem_map);
+
+	rs232_port_device &rs232(RS232_PORT(config, "rs232", default_rs232_devices, "terminal"));
+	rs232.rxd_handler().set("ser", FUNC(serial_port_device::rx));
+
+	serial_port_device &ser(SERIAL_PORT(config, "ser", 0));
+	ser.tx_handler().set("rs232", FUNC(rs232_port_device::write_txd));
 }
 
 ROM_START( octane )
@@ -63,6 +101,4 @@ ROM_END
 
 } // anonymous namespace
 
-
-//    YEAR  NAME      PARENT  COMPAT  MACHINE   INPUT    CLASS          INIT        COMPANY                 FULLNAME                                 FLAGS
-COMP( 1997, octane,   0,      0,      octane,   octane,  octane_state,  empty_init, "Silicon Graphics Inc", "Octane (Version 6.5 Rev 4.9 05/22/03)", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
+COMP( 1997, octane, 0, 0, octane, octane, octane_state, empty_init, "Silicon Graphics Inc", "Octane (Version 6.5 Rev 4.9 05/22/03)", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
